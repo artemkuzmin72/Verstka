97de36d60daf2ffb1af3802391f092c40b834034
Artem Kuzmin <frankiekoo72@mail.ru>
2025-09-04 00:07:16 +0300

    Second

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
 
+hostName = "localhost"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200) # Отправка кода ответа
+        self.send_header("Content-type", "application/json") # Отправка типа данных, который будет передаваться
+        self.end_headers() # Завершение формирования заголовков ответа
+        self.wfile.write(bytes("{'message': 'OK'}", "utf-8")) # Тело ответа
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
